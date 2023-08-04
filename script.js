040d2f15490880c586877c1c44ce513f030fb1e8
Joel Foust <joel.foust@yahoo.com>
2023-08-04 14:07:43 -0400

    Final Assignment 4

--- script.js
+++ script.js
@@ -20,6 +20,51 @@ window.addEventListener('load', function (e) {
     });
 });
 
+function reveal() {
+
+    let revealLeft = document.querySelectorAll('.wrapper-Left');
+    let revealRight = document.querySelectorAll('.wrapper-Right');
+
+    for (let i = 0; i < revealLeft.length; i++) {
+
+        let windowHeight = window.innerHeight;
+        let revealTop = revealLeft[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+
+            revealLeft[i].classList.add('active');
+
+        } else {
+
+            revealLeft[i].classList.remove('active');
+
+        }
+
+    }
+
+    for (let i = 0; i < revealRight.length; i++) {
+
+        let windowHeight = window.innerHeight;
+        let revealTop = revealRight[i].getBoundingClientRect().top;
+        let revealPoint = 150;
+
+        if (revealTop < windowHeight - revealPoint) {
+
+            revealRight[i].classList.add('active');
+
+        } else {
+
+            revealRight[i].classList.remove('active');
+
+        }
+
+    }
+
+
+}
+
+window.addEventListener('scroll', reveal);
 
 
 
